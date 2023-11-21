b1590ef0d50595f0e921e3b46d604a8461576cd4
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-11-21 20:20:50 +0530

    Server error handling

--- task-manager-be-api/src/server.js
+++ task-manager-be-api/src/server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = () => {
   /* CREATE and LISTEN Server */
-  app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
+  app.listen(PORT, (err) => {
+    if (!err) {
+      console.log(`Server listening on PORT: ${PORT}`);
+    } else {
+      console.log(`Some Error Enconuntered to run the Server: ${err}`);
+    }
   });
 };
 
