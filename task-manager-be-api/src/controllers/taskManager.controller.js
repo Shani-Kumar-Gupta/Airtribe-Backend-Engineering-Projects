eae4a4894423cdc8474cae4ed0b06d698fe601b0
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-11-22 18:41:37 +0530

    Build fetch API to fetch task based on task id

--- task-manager-be-api/src/controllers/taskManager.controller.js
+++ task-manager-be-api/src/controllers/taskManager.controller.js
@@ -4,7 +4,15 @@ function getTasks(req, res, next) {
   return res.status(200).send(tasksData);
 }
 
-function getByTaskId(req, res, next) { }
+function getByTaskId(req, res, next) {
+  let taskId = req.params.taskId;
+  let result = tasksData.tasks.filter(task => task.taskId == taskId);
+  if (result.length) {
+    return res.status(200).send(result);
+  } else {
+    return res.status(404).send('Task not found');
+  }
+}
 
 function createTask(req, res, next) { }
 
