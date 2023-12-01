093fd3a749200edab0d1e7638ae26961de21fbb7
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-12-01 16:58:10 +0530

    Minor fixes

--- news-aggregator-api/src/services/newsapi.service.js
+++ news-aggregator-api/src/services/newsapi.service.js
@@ -2,9 +2,10 @@ const { default: axios } = require('axios');
 
 /* News API tto fetch News data using promises */
 function fetchNews(url) {
+  console.log("news api url: ", url);
   return new Promise((resolve, reject) => {
     axios.get(url).then(resp => {
-      return resolve(resp.data);
+      return resolve(resp);
     }).catch(err => {
       return reject(err);
     });
